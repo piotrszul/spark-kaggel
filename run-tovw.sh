748af706e3989bc6dcff5086e7ba8bfff92e8b2c
Piotr Szul <piotr.szul@csiro.au>
2014-08-19 18:39:15 +1000

    Modified

--- run-tovw.sh
+++ run-tovw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-hadoop fs -rm -r -f kaggel/sample_6p_1_20p.vw
-spark-submit --master yarn-client --num-executors 48 --driver-memory 4G --executor-memory 6G  --class df.DataFrameApp  target/scala-2.10/spark-kaggel_2.10-1.0.jar kaggel/sample_6p_1_20p.csv kaggel/sample_6p_1_20p.vw 0 
+hadoop fs -rm -r -f kaggel/train.vw
+spark-submit --master yarn-client --num-executors 48 --driver-memory 4G --executor-memory 6G  --class df.DataFrameApp  target/scala-2.10/spark-kaggel_2.10-1.0.jar kaggel/train.csv kaggel/train.vw 0 
 
